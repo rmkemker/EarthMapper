61141e0668a0d9bef1121e96665dad58f05859ae
klab02 <rmk6217@rit.edu>
2018-04-18 08:28:31 -0400

    More cleanup from legacy code

--- utils/utils.py
+++ utils/utils.py
@@ -11,8 +11,7 @@ http://www.gdal.org/
 
 import numpy as np
 from spectral import BandResampler
-
-import sys, os
+import os
 
 def band_resample_hsi_cube(data, bands1, bands2, fwhm1,fwhm2 , mask=None):
     """
@@ -51,43 +50,6 @@ def band_resample_hsi_cube(data, bands1, bands2, fwhm1,fwhm2 , mask=None):
     out[np.isnan(out)] = 0
     return out.reshape((dataSize[0],dataSize[1],len(bands2)))
 
-    
-from sklearn.model_selection import train_test_split
-
-def class_weights(labels, mu , numClasses=None):
-    if numClasses is None:
-        numClasses= np.max(labels)+1    
-    class_weights = np.bincount(labels, minlength=numClasses)       
-    class_weights = np.sum(class_weights)/class_weights
-    class_weights[np.isinf(class_weights)] = 0
-    class_weights = mu * np.log(class_weights)
-    class_weights[class_weights < 1] = 1
-    return class_weights
-
-
-def train_test_split_per_class(X, y, train_size=None, test_size=None):
-    
-    sh = np.array(X.shape)
-    
-    num_classes = len(np.bincount(y))
-    
-    sh[0] = 0
-    X_train_arr =  np.zeros(sh, dtype=X.dtype)
-    X_test_arr = np.zeros(sh, dtype=X.dtype)
-    y_train_arr = np.zeros((0), dtype=y.dtype)
-    y_test_arr = np.zeros((0), dtype=y.dtype)
-
-    for i in range(num_classes):
-        X_train, X_test, y_train, y_test = train_test_split(X[y==i], y[y==i],
-                                                            train_size=train_size,
-                                                            test_size=test_size)
-        
-        X_train_arr =  np.append(X_train_arr, X_train, axis=0)
-        X_test_arr = np.append(X_test_arr, X_test, axis=0)
-        y_train_arr = np.append(y_train_arr, y_train)
-        y_test_arr = np.append(y_test_arr, y_test)
-        
-    return X_train_arr, X_test_arr, y_train_arr, y_test_arr
 
 def set_gpu(device=None):
     
